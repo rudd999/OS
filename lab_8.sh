7d36c4902cf49bc2441f9fc194e771c5a4779ba8
rudd999 <45066205+rudd999@users.noreply.github.com>
2020-05-06 11:51:56 +0300

    Add files via upload

---
+++ lab_8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls /home/$rudolf
+ls -l /home/$rudolf
+ls -al /home/$rudolf
+touch ~/labs/8/new.file
+echo $rudolf > ~/labs/8/new.file
+chmod -w ~/labs/8/new.file
+echo $rudolf > ~/labs/8/new.file
+chmod +w ~/labs/8/new.file
+
